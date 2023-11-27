f02dc5b9071a26b0a5a69aab035d9237889e6728
Charbel Elnacouzi <boulina2@hotmail.com>
2023-11-27 04:40:13 -0800

    https://github.com/Charbelelnacouzi/elementals.git

--- src/Page6.jsx
+++ src/Page6.jsx
@@ -37,7 +37,7 @@ const Page6 = () => {
                     >
                         The elements airdrop
                     </h1>
-                    <div style={{ height: "120vh", overflow: "auto" }}>
+                    <div style={{ height: "90vh", overflow: "auto" }}>
                         <div
                             style={{
                                 fontFamily: "Kanit",

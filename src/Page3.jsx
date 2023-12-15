ba27e6236897f10798fa39111563c20fff6133c3
Charbel Elnacouzi <boulina2@hotmail.com>
2023-12-15 18:25:37 +0200

    https://github.com/Charbelelnacouzi/elementals.git

--- src/Page3.jsx
+++ src/Page3.jsx
@@ -98,7 +98,7 @@ const Page3 = () => {
                         style={{
                             paddingLeft: "20px",
                             margin: "1rem 0",
-                            padding: "2rem",
+                            padding: "5rem",
                         }}
                     >
                         <CharacterGallery style={{ width: "100px" }} />

df75d6bef567ae05ce73a8410c33058e91510130
Charbel Elnacouzi <boulina2@hotmail.com>
2023-12-15 18:29:45 +0200

    https://github.com/Charbelelnacouzi/elementals.git

--- src/Page3.jsx
+++ src/Page3.jsx
@@ -39,7 +39,7 @@ const Page3 = () => {
                     >
                         Vision
                     </h1>
-                    <div style={{ height: "60vh" }}>
+                    <div style={{ height: "60vh", overflow:"auto" }}>
                         <div
                             style={{
                                 fontFamily: "Kanit",
